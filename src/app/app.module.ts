103c429b707e251cf4b05b8fb0b5d24fc5db8246
Liliia Nedashkivska <liliia.nedashkivska@nixsolutions.com>
2018-12-13 11:52:14 +0200

    add header component; add router link on header menu bar;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { MatTabsModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,16 +7,19 @@ import { HomeModule } from "./components/home/home.module";
 import { UsersModule } from './components/users/users.module';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     UsersModule,
     HomeModule,
+    MatTabsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -1,7 +1,4 @@
 import { NgModule } from '@angular/core';
-import { MatTabsModule } from '@angular/material';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HomeComponent } from './home.component';
 
@@ -9,11 +6,7 @@ import { HomeComponent } from './home.component';
   declarations: [
     HomeComponent,
   ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatTabsModule,
-  ],
+  imports: [],
   providers: [],
 })
 export class HomeModule { }
