b3361991e4ba71ab4c6b6003e51cee32bc45f5b6
Liliia Nedashkivska <liliia.nedashkivska@nixsolutions.com>
2018-12-17 16:06:13 +0200

    add change detection strategy for child components;

--- src/app/components/users/users-list/user-item/user-item.component.ts
+++ src/app/components/users/users-list/user-item/user-item.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 
 import { IUser } from '../../../../models/user.model';
 
 @Component({
   selector: 'app-user-item',
   templateUrl: './user-item.component.html',
-  styleUrls: ['./user-item.component.scss']
+  styleUrls: ['./user-item.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserItemComponent implements OnInit {
   @Input() user: IUser;
--- src/app/components/users/users-list/users-list.component.ts
+++ src/app/components/users/users-list/users-list.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 
 import { IUser } from '../../../models/user.model';
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
-  styleUrls: ['./users-list.component.scss']
+  styleUrls: ['./users-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UsersListComponent implements OnInit {
   @Input() users: Array<IUser>;
